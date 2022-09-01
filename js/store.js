65edca055bd19b664a01754dc4939401038c3192
Christian Fiorentino <cfiorentino@gmail.com>
2022-09-01 20:01:48 -0300

    +funcion btn Comprar(store)

--- js/store.js
+++ js/store.js
@@ -29,7 +29,7 @@ setTimeout( function() {
                     <div class="d-flex justify-content-between align-items-center">
                         <small class="text-white precio">U$D ${producto.precio}</small>
                         <div class="btn-group">
-                            <button type="button" class="btn btn-sm btn-outline-secondary btn-custom1">
+                            <button type="button" data-bs-toggle="modal" data-bs-target="#modalCarrito" class="comprar btn btn-sm btn-outline-secondary btn-custom1" id="c${producto.id}">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gem" viewBox="0 0 16 16">
                                     <path d="M3.1.7a.5.5 0 0 1 .4-.2h9a.5.5 0 0 1 .4.2l2.976 3.974c.149.185.156.45.01.644L8.4 15.3a.5.5 0 0 1-.8 0L.1 5.3a.5.5 0 0 1 0-.6l3-4zm11.386 3.785-1.806-2.41-.776 2.413 2.582-.003zm-3.633.004.961-2.989H4.186l.963 2.995 5.704-.006zM5.47 5.495 8 13.366l2.532-7.876-5.062.005zm-1.371-.999-.78-2.422-1.818 2.425 2.598-.003zM1.499 5.5l5.113 6.817-2.192-6.82L1.5 5.5zm7.889 6.817 5.123-6.83-2.928.002-2.195 6.828z"/>
                                 </svg>   Comprar
@@ -50,13 +50,20 @@ setTimeout( function() {
         }
     }
     console.log("Render de Productos Finalizado")
+    
     // Evento para botones Agregar
     let botones = document.getElementsByClassName('agregar')
     for (let i=0; i<botones.length; i++){
         botones[i].addEventListener('click', agregarAlCarrito)
         //botones[i].addEventListener('click', console.log("click"))
     }
-    
+
+    // Evento para botones Comprar
+    let botonesC = document.getElementsByClassName('comprar')
+    for (let i=0; i<botonesC.length; i++){
+        botonesC[i].addEventListener('click', comprar)
+    }
+
     let totalCarr = -100
     let promo = 100
     let articulosEnCarro = []
@@ -101,9 +108,32 @@ setTimeout( function() {
         //--------------------------------------------------------
         
         /**
+         * Evento para Comprar
+         */
+        function comprar(e) {
+            let idc = e.target.id[1]
+            console.log(e.target.id[1])
+            // Agrego el Nodo al carrito
+            let artPorAgregar = productos.find(producto => producto.id == e.target.id[1])
+            console.log("Agregando: " +artPorAgregar)
+            artCarrito.push(artPorAgregar.id)
+            console.log("Articulos en carrito :"+artCarrito)
+            
+            //Reseteo el codigo de descuento para forzar el recalculo
+            document.getElementById('codeform').reset()
+            document.getElementById('desc').innerText = 0
+            // Actualizamos el carrito 
+            renderizarCarrito();
+            // Actualizar info en localStorage
+            guardarCarritoEnLocalStorage();
+
+        }
+
+        /**
          * Evento para agregar un producto al carrito
          */
         function agregarAlCarrito(e) {
+            console.log(e.target)
             // Agrego el Nodo al carrito
             let artPorAgregar = productos.find(producto => producto.id == e.target.id)
             console.log("Agregando: " +artPorAgregar)
